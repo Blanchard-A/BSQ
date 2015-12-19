8ca53132ceb132898d421973c0971286bc2f02c3
Alexandre Blanchard <alexandre.blanchard@epitech.eu>
2015-12-19 19:42:42 +0100

    Push final

--- count.c
+++ count.c
@@ -5,30 +5,13 @@
 ** Login   <blanch_p@epitech.net>
 ** 
 ** Started on  Wed Dec 16 11:12:41 2015 Alexandre Blanchard
-** Last update Fri Dec 18 14:37:12 2015 Alexandre Blanchard
+** Last update Sat Dec 19 19:42:23 2015 Alexandre Blanchard
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-/* int	nb_lgne(char *file, int i, int y) */
-/* { */
-/*   char	buffer[4096]; */
-/*   int	fd; */
-
-/*   fd = open(file, O_RDONLY); */
-/*   read(fd, buffer, 4096); */
-/*   while (buffer[i] != 0) */
-/*     { */
-/*       if (buffer[i] == '\n') */
-/* 	y++; */
-/*       i++; */
-/*     } */
-/*   close(fd); */
-/*   return (y); */
-/* } */
 
 int	nb_colonne(char *file, int i, int y)
 {
